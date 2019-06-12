7a2eb16def13575b22251207c46b13a47a1a0c5c
Marcus Turnbo <marcus.turnbo@morningstar.com>
2019-06-12 15:10:59 -0500

    Split OHLC and Volume in stock chart

--- src/components/Chart/StockChart.js
+++ src/components/Chart/StockChart.js
@@ -1,36 +1,83 @@
 import React from 'react';
 import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
-import data from '../../data/aapl-ohlc.json';
+import data from '../../data/aapl-ohlcv.json';
 
-const options = {
-  title: {
-    text: 'AAPL Stock Price'
-  },
-  rangeSelector: {
-    selected: 2
-  },
-  series: [{
-    type: 'ohlc',
-    dateFormat: 'YYYY-mm-dd',
-    name: 'AAPL Stock Price',
-    data,
-    dataGrouping: {
-      units: [[
-        'week', // unit name
-        [1] // allowed multiples
-      ], [
-        'month',
-        [1, 2, 3, 4, 6]
-      ]]
-    }
-  }]
+const StockChart = () => {
+  const groupingUnits = [
+    [
+      'week',
+      [1]
+    ],
+    [
+      'month',
+      [1, 2, 3, 4, 6]
+    ]
+  ];
+
+  const options = {
+    rangeSelector: {
+      selected: 1
+    },
+    title: {
+      text: 'AAPL Historical'
+    },
+    yAxis: [{
+      labels: {
+        align: 'right',
+        x: -3
+      },
+      title: {
+        text: 'OHLC'
+      },
+      height: '60%',
+      lineWidth: 2,
+      resize: {
+        enabled: true
+      }
+    }, {
+      labels: {
+        align: 'right',
+        x: -3
+      },
+      title: {
+        text: 'Volume'
+      },
+      top: '65%',
+      height: '35%',
+      offset: 0,
+      lineWidth: 2
+    }],
+    tooltip: {
+      split: true
+    },
+    series: [{
+      type: 'candlestick',
+      name: 'AAPL',
+      data: [],
+      dataGrouping: {
+        units: groupingUnits
+      }
+    }, {
+      type: 'column',
+      name: 'Volume',
+      data: [],
+      yAxis: 1,
+      dataGrouping: {
+        units: groupingUnits
+      }
+    }]
+  };
+
+  // extract ohlc and volume from data
+  options.series[0].data = data.map(row => row.slice(0,5));
+  options.series[1].data = data.map(row => [row[0], row[5]]);
+
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} constructorType="stockChart" options={options} />
+    </div>
+  );
 };
-
-const StockChart = () => (
-  <div>
-    <HighchartsReact highcharts={Highcharts} constructorType="stockChart" options={options} />
-  </div>
-);
 
 export default StockChart;
